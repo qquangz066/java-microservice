dfb69fea2f4cc67a0a9d07a5a494e79f4aa67ddb
QuangLN1 <QuangLN1@fsoft.com.vn>
2019-02-12 17:09:09 +0700

    add traefik

--- uaa/src/main/java/com/example/controller/HomeController.java
+++ uaa/src/main/java/com/example/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,10 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("home")
 public class HomeController {
+    @Autowired
+    Environment environment;
 
     @GetMapping
     @PreAuthorize("#oauth2.clientHasRole('user:read')")
     public String index(){
         return "testhome";
+    }
+
+    @GetMapping("port")
+    public String indax(){
+        return environment.getProperty("local.server.port");
     }
 }
